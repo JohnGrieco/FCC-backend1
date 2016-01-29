259bd5afc251e77ec7e3abc235faf70480067742
johngrieco <johnjgrieco@gmail.com>
2016-01-29 05:27:20 +0000

    added favicon

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var express = require('express');
 var app = express();
 var path = require("path");
 var sugar = require("sugar");
+var favicon = require("serve-favicon");
 
 app.use(express.static('public'));
-
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
 	
