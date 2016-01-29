1d8005df9af7aa7b19132d27f7d065f96d456a26
johngrieco <johnjgrieco@gmail.com>
2016-01-29 04:49:49 +0000

    blah

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/:date', function(req, res){
 
 
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+	console.log('Node.js pissing on port ' + port + '...');
 });
